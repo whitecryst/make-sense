3ae36d798a209f94ea8d124d5a44aadc9c227462
Kolja Bailly <baill@MacBook-Pro.fritz.box>
2021-03-05 00:07:44 +0100

    add all files

---
+++ src/server/server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('<html><body><h1>Hello World</h1></body></html>');
+});
+
+app.post('/submit-data', function (req, res) {
+    res.send('POST Request');
+});
+
+app.put('/update-data', function (req, res) {
+    res.send('PUT Request');
+});
+
+app.delete('/delete-data', function (req, res) {
+    res.send('DELETE Request');
+});
+
+//setting middleware
+app.use('/images', express.static(__dirname + '/Images'));
+
+
+var server = app.listen(5000);
---
+++ src_bak/utils/FileUtil.ts
@@ -0,0 +1,51 @@
+export class FileUtil {
+    public static loadImage(fileData: File): Promise<HTMLImageElement> {
+		return new Promise((resolve, reject) => {
+			const url = URL.createObjectURL(fileData);
+            const image = new Image();
+			image.src = url;
+			image.onload = () => resolve(image);
+			image.onerror = reject;
+		})
+    }
+
+    public static loadImages(fileData: File[]): Promise<HTMLImageElement[]> {
+        return new Promise((resolve, reject) => {
+            const promises: Promise<HTMLImageElement>[] = fileData.map((fileData: File) => FileUtil.loadImage(fileData))
+            Promise
+                .all(promises)
+                .then((values: HTMLImageElement[]) => resolve(values))
+                .catch((error) => reject(error));
+        });
+    }
+
+    public static readFile(fileData: File): Promise<string> {
+        return new Promise((resolve, reject) => {
+            let reader = new FileReader();
+            reader.onloadend = (event: any) => {
+                resolve(event.target.result);
+            };
+            reader.onerror = reject;
+            reader.readAsText(fileData);
+        })
+    }
+
+    public static readFiles(fileData: File[]): Promise<string[]> {
+        return new Promise((resolve, reject) => {
+            const promises: Promise<string>[] = fileData.map((fileData: File) => FileUtil.readFile(fileData))
+            Promise
+                .all(promises)
+                .then((values: string[]) => resolve(values))
+                .catch((error) => reject(error));
+        });
+    }
+
+    public static extractFileExtension(name: string): string | null {
+        const parts = name.split(".");
+        return parts.length > 1 ? parts[1] : null;
+    }
+
+    public static extractFileName(name: string): string | null {
+        return name.split(".")[0];
+    }
+}
